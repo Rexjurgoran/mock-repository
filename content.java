623ea46372ee8c5d47c2e94124c48e4377202f25
Marvin Henßler <marvinhenssler@aol.com>
2020-12-02 13:56:04 +0000

    add code in files types: .java,.properties remove code in files types: .properties,.java

--- content.java
+++ content.java
@@ -4,6 +4,6 @@ public class Cjlgmq {
         System.out.println("xdkkd");
         System.out.println("noqwi");
         System.out.println("lxurm");
-        System.out.println("inpjf");
+        System.out.println("xdbyp");
     }
 }
