0f0e902469a1605b24a37694561ba7c5bc00acc9
Marvin Henßler <marvinhenssler@aol.com>
2021-04-13 13:30:17 +0000

    add code in files types: .properties,.xml,.java,.tag remove code in files types: .properties,.tag

--- content.java
+++ content.java
@@ -64,5 +64,6 @@ public class Cjlgmq {
         System.out.println("uhbno");
         System.out.println("veodm");
         System.out.println("jarld");
+        System.out.println("xfmri");
     }
 }
