28766821d64933a073b0bfc550f5f956daa70461
Marvin Henßler <marvinhenssler@aol.com>
2021-05-03 12:48:54 +0000

    add code in files types: .java,.properties,.tag remove code in files types: .java,.properties,.tag

--- content.java
+++ content.java
@@ -36,6 +36,6 @@ public class Cjlgmq {
         System.out.println("snvhn");
         System.out.println("xtdfc");
         System.out.println("nuuyo");
-        System.out.println("lcpvi");
+        System.out.println("qkqkf");
     }
 }
