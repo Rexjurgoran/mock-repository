389ec15115fcb68061435ce1e636d9427f8fcca5
Marvin Henßler <marvinhenssler@aol.com>
2021-05-17 06:23:34 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -36,6 +36,6 @@ public class Cjlgmq {
         System.out.println("snvhn");
         System.out.println("xtdfc");
         System.out.println("nuuyo");
-        System.out.println("nydvt");
+        System.out.println("wadnx");
     }
 }
