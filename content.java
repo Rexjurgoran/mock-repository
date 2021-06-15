38d2e8e94122840f0dda1b559281ebb2f9743d17
Marvin Henßler <marvinhenssler@aol.com>
2021-06-15 09:42:47 +0000

    add code in files types: .xml,.java remove code in files types:

--- content.java
+++ content.java
@@ -14,5 +14,6 @@ public class Cjlgmq {
         System.out.println("aorlk");
         System.out.println("ohnvh");
         System.out.println("wyjki");
+        System.out.println("apbgk");
     }
 }
