badc9c336646f9298c38c8269d7a77ad22239fa4
Marvin Henßler <marvinhenssler@aol.com>
2021-06-17 10:53:13 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -37,6 +37,6 @@ public class Cjlgmq {
         System.out.println("xtdfc");
         System.out.println("nuuyo");
         System.out.println("mckwg");
-        System.out.println("jfkqj");
+        System.out.println("wodrv");
     }
 }
