58a51cdff981ce12a372c0125034615782443dbc
Marvin Henßler <marvinhenssler@aol.com>
2021-10-04 10:46:01 +0000

    add code in files types: ,.launch,.xml,.java,.properties,.number,.jsp,.xmi,.css remove code in files types:

--- content.java
+++ content.java
@@ -18,5 +18,6 @@ public class Cjlgmq {
         System.out.println("nsjcm");
         System.out.println("ekend");
         System.out.println("kvkpn");
+        System.out.println("blhnm");
     }
 }
