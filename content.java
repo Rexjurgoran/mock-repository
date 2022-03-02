1f6a6572fbc1c09f04a757b032d6d10d16c7805d
Marvin Henßler <marvinhenssler@aol.com>
2022-03-02 12:55:38 +0000

    add code in files types: .java,.xml,.scss,.properties,.tag,.css,.js,.jsp remove code in files types: .java,.xml,.scss,.properties,.tag,.css,.js,.jsp

--- content.java
+++ content.java
@@ -20,6 +20,6 @@ public class Cjlgmq {
         System.out.println("kvkpn");
         System.out.println("kmnss");
         System.out.println("pmffm");
-        System.out.println("sktna");
+        System.out.println("qblhy");
     }
 }
