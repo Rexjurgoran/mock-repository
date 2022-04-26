cb44484016194d98913369ec448f287509ea7c4a
Marvin Henßler <marvinhenssler@aol.com>
2022-04-26 06:50:42 +0000

    add code in files types: .properties,.java,.tag,.jsp,.js,.xml,,.scss,.css,.impex remove code in files types: .properties,.java,.tag,.jsp,.js,,.scss,.css,.impex,.xml

--- content.java
+++ content.java
@@ -49,6 +49,6 @@ public class Cjlgmq {
         System.out.println("nfryf");
         System.out.println("wigam");
         System.out.println("mljpy");
-        System.out.println("ggbwe");
+        System.out.println("hfogs");
     }
 }
