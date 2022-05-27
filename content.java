18f6d30d3989108e77129ec6fb93f76f86d03ca1
Marvin Henßler <marvinhenssler@aol.com>
2022-05-27 07:13:37 +0000

    add code in files types: .java,.jsp,.js remove code in files types: .java,.jsp,.js

--- content.java
+++ content.java
@@ -79,6 +79,6 @@ public class Cjlgmq {
         System.out.println("fesqi");
         System.out.println("akmyo");
         System.out.println("ampdr");
-        System.out.println("mcssu");
+        System.out.println("iwyxu");
     }
 }
