fd1457358b4e4a3017fd2b11f734a5fef3f4a5ba
Marvin Henßler <Marvin.Henssler@HARTING.com>
2023-11-09 06:51:33 +0000

    add code in files types: .xml,.java,.impex remove code in files types: .xml,.java,.impex

--- content.java
+++ content.java
@@ -89,6 +89,6 @@ public class Cjlgmq {
         System.out.println("nkcrb");
         System.out.println("tnfch");
         System.out.println("jkhhk");
-        System.out.println("kqumr");
+        System.out.println("vdhkj");
     }
 }
